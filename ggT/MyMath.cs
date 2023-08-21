af5d403530f40ba31d77ab54bde0bacb0d75cdc1
ClaudeMathys <claude.mathys@ksb-sg.ch>
2023-08-21 12:25:23 +0200

    #1

---
+++ ggT/MyMath.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ggT
+{
+    public class MyMath
+    {
+        public static int Calc_ggT(int ersteZahl, int zweiteZahl)
+        {
+            while (zweiteZahl != 0)
+            {
+                rggT = 
+            }
+            return rggT;
+        }
+    }
+}
