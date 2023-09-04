a518f20c1f6191aed6bf9dc0de964dcbb49d1516
ClaudeMathys <claude.mathys@ksb-sg.ch>
2023-09-04 09:58:07 +0200

    1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle1 = new Rectangle(7, 8);
+            Rectangle rectangle2 = new Rectangle(7, 8, true);
+            Rectangle rectangle3 = new Rectangle(7, 8, System.Drawing.Color.Blue, false);
+            Rectangle rectangle4 = new Rectangle(7, 8, System.Drawing.Color.Green);
+            Console.ReadLine();
+        }
+    }
+}
