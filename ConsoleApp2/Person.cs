d478a6c1bf438f4c8495a2efe6d7edcd21129fcd
ClaudeMathys <claude.mathys@ksb-sg.ch>
2023-09-04 12:16:25 +0200

    1

---
+++ ConsoleApp2/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Person
+    {
+
+        private string Name;
+        private int WutMeter;
+
+        public int Provozieren()
+        {
+            WutMeter++;
+            return WutMeter;
+        }
+
+        public int HerunterFahren()
+        {
+            WutMeter--;
+            return WutMeter;
+        }
+        public int KopfAnschlagen()
+        {
+            WutMeter++;
+            return WutMeter;
+        }
+
+        public int BierTrinken()
+        {
+            WutMeter--;
+            return WutMeter;
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,13 +4,11 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Rectangle rectangle1 = new Rectangle(7, 8);
-            Rectangle rectangle2 = new Rectangle(7, 8, true);
-            Rectangle rectangle3 = new Rectangle(7, 8, System.Drawing.Color.Blue, false);
-            Rectangle rectangle4 = new Rectangle(7, 8, System.Drawing.Color.Green);
-            Console.ReadLine();
+            Person.ReferenceEquals
+            Person person = new Person();
         }
     }
 }
